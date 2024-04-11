8553dc1b542d5f9796b9975d6803713fb38783fe
Temitope <ahmed.temitopesodiq@gmail.com>
2024-04-11 09:51:01 +0100

    first trial

---
+++ example1.sh
@@ -0,0 +1,12 @@
+!#/bin/zsh
+
+#Simple password generator
+
+echo"This is a simple password generator"
+echo"please enter the lenght of the password"
+read PASS_LENGTH
+
+for p in $(seq 1 5);
+do
+	openssl rand -base64| 48 | cut -c1-$PASS_LENGTH
+done
